fc27ad85d11d6802a2fcf2710f41f036ebc32e8f
Marina Bajic <marinabajic10@gmail.com>
2023-09-03 19:37:23 +0200

    Deleted unused code from the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,4 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
-
-exports.default = defaultTask;
-
 const gulp = require('gulp');
-const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 // const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
@@ -14,24 +6,6 @@ const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('taskCSS', async () => {
     return gulp.src([
-        // './dev/abstracts/_breakpoints.scss',
-        // './dev/abstracts/_colors.scss',
-        // './dev/abstracts/_mixins.scss',
-        // './dev/abstracts/_typography.scss',
-        // './dev/abstracts/_variables.scss',
-        // './dev/base/_general.scss',
-        // './dev/base/_reset.scss',
-        // './dev/components/_buttons.scss',
-        // './dev/components/_card.scss',
-        // './dev/components/_headings.scss',
-        // './dev/components/_image-with-text-module.scss',
-        // './dev/components/_navigation.scss',
-        // './dev/components/_signature.scss',
-        // './dev/components/_socials.scss',
-        // './dev/layout/_grid.scss',
-        // './dev/layout/_header.scss',
-        // './dev/layout/_sections.scss',
-        // './dev/pages/landing-page.scss'
         './dev/main.scss'
     ])
         .pipe(concat('dist.main.scss'))
