8b2f2e79c100ce40e6e769eb5aef7d93f56cfc60
MarinaBajic <marinabajic10@gmail.com>
2024-07-16 19:26:01 +0200

    More responsiveness

--- src/sections/experience/Experience.tsx
+++ src/sections/experience/Experience.tsx
@@ -3,7 +3,7 @@ import styles from './Experience.module.scss';
 const Experience = () => {
 	return (
 		<section className={`section`}>
-			<span className="background">Skills</span>
+			<span className="background">Experience</span>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse at obcaecati earum dolorum molestias veniam quod quis sapiente, cupiditate perspiciatis enim maxime ipsa, quos culpa, mollitia nemo laudantium totam expedita?</p>
 		</section>
 	);
