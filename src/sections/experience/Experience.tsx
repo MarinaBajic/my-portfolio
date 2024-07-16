be13bf0e6bc234570228dd92b0a7323337327fab
MarinaBajic <marinabajic10@gmail.com>
2024-07-16 19:18:21 +0200

    More fixing of background text

--- src/sections/experience/Experience.tsx
+++ src/sections/experience/Experience.tsx
@@ -1,7 +1,12 @@
 import styles from './Experience.module.scss';
 
 const Experience = () => {
-	return <section className={`section`}>gergregr</section>;
+	return (
+		<section className={`section`}>
+			<span className="background">Skills</span>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse at obcaecati earum dolorum molestias veniam quod quis sapiente, cupiditate perspiciatis enim maxime ipsa, quos culpa, mollitia nemo laudantium totam expedita?</p>
+		</section>
+	);
 };
 
 export default Experience;
