51b507ef551b2d37dfee9ea854908aeba124c3f4
MarinaBajic <marinabajic10@gmail.com>
2024-07-18 21:54:17 +0200

    Finished abput me section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,6 +1,8 @@
 import styles from './About.module.scss';
 import Section from '../section/Section';
 import Title from '../title/Title';
+import profilePicture from '../../assets/marina.png';
+import curvedText from '../../assets/curved-text.svg';
 
 const About = () => {
 	return (
@@ -10,12 +12,44 @@ const About = () => {
 			subheading="About me"
 			heading="Dedicated Front-end Developer"
 		>
-			<div className={styles.content}>
-				<Title
-					subtitle="Hello, my name is"
-					title="Marina Bajić"
-					color="gold"
-				/>
+			<div className={styles.about}>
+				<div className={styles.content}>
+					<Title
+						subtitle="Hello, my name is"
+						title="Marina Bajić"
+						color="gold"
+					/>
+					<div className={styles.text}>
+						<p>
+							Creating high-quality code and design is what I do.
+						</p>
+						<p>
+							Recent graduate in Informatics – Information
+							Technologies from the Faculty of Sciences,
+							University of Novi Sad, Serbia, volunteering in a
+							non-governmental organization.
+						</p>
+						<p>
+							Loves going to hackathons, designing and developing
+							web applications, reading, sewing, and solving
+							puzzles.
+						</p>
+					</div>
+				</div>
+				<div className={styles.profile}>
+					<img
+						className={styles.image}
+						src={profilePicture}
+						alt="Marina Bajić"
+						loading="lazy"
+					/>
+					<img
+						className={styles.text}
+						src={curvedText}
+						alt="Dedicated Front-end Developer"
+						loading="lazy"
+					/>
+				</div>
 			</div>
 		</Section>
 	);
