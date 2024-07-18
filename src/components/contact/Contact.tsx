9dfe7466801aea3924fc2e6f4ceca82fe5e474b7
MarinaBajic <marinabajic10@gmail.com>
2024-07-18 22:57:58 +0200

    Added contact section

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,5 +1,6 @@
 import styles from './Contact.module.scss';
 import Section from '../section/Section';
+import logo from '../../assets/logo-gold.svg';
 
 const Contact = () => {
 	return (
@@ -9,7 +10,26 @@ const Contact = () => {
 			subheading="Contact me"
 			heading="How can I help?"
 		>
-			Test
+			<div className={styles.card}>
+				<div className={styles.content}>
+					<div className={styles.front}>
+						<img className={styles.logo} src={logo} alt="Logo" />
+					</div>
+					<a
+						className={styles.back}
+						href="mailto:marinabajic10@gmail.com"
+					>
+						<img className={styles.logo} src={logo} alt="Logo" />
+						<div className={styles.signature}>
+							<span className={styles.name}>Marina Bajić</span>
+							<span>Front-end Developer</span>
+						</div>
+						<span className={styles.email}>
+							marinabajic10@gmail.com
+						</span>
+					</a>
+				</div>
+			</div>
 		</Section>
 	);
 };
