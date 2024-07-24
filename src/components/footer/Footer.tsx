4a446178ca799e9928944525fb9c52e7936848e3
MarinaBajic <marinabajic10@gmail.com>
2024-07-25 00:20:27 +0200

    New footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,8 @@
-import Background from '../background/Background';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
 	return (
 		<footer className={styles.footer}>
-			<Background style={styles.background} text="Marina" />
 			<div className={styles.copyright}>
 				<span>Made with love by Marina Bajić</span>
 				<span>Copyright 2023</span>
