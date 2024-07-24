cde1bbd0d3501d713ffd79095166abf6f038dde9
MarinaBajic <marinabajic10@gmail.com>
2024-07-24 22:57:21 +0200

    Refactoring

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Title = ({ subtitle, title }: Props) => {
 	return (
-		<div className={`${styles.container}`}>
+		<div className={`${styles.content}`}>
 			<span className={styles.subtitle}>{subtitle}</span>
 			<span className={styles.title}>{title}</span>
 		</div>
