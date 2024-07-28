9f97e50cfb26fba45bee471b4d29a75083768688
MarinaBajic <marinabajic10@gmail.com>
2024-07-28 13:46:18 +0200

    Fix small media

--- src/utils/mediaQueryUtils.tsx
+++ src/utils/mediaQueryUtils.tsx
@@ -1,7 +1,7 @@
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 const LARGE_MEDIA_QUERY = '(min-width: 1440px)';
-const SMALL_MEDIA_QUERY = '(max-width: 833px)';
+const SMALL_MEDIA_QUERY = '(max-width: 768px)';
 
 export const isSmallScreen = () => {
 	return useMediaQuery(SMALL_MEDIA_QUERY);
