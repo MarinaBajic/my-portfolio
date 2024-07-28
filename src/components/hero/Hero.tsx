73222b7fa8830a782f5d2037a2ac55eba60f3a81
MarinaBajic <marinabajic10@gmail.com>
2024-07-28 19:39:12 +0200

    Refactored

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,44 +2,19 @@ import styles from './Hero.module.scss';
 import Button from '../buttons/Button';
 import Slider from '../slider/Slider';
 import Background from '../background/Background';
-import { useTheme } from '../../context/ThemeProvider';
 
 const Hero = () => {
-	const { theme } = useTheme();
-
 	return (
 		<section id="hero" className={styles.hero}>
 			<div className={styles.banner}>
-				<Background
-					text="marina"
-					style={`${styles.background} ${
-						styles[`background--${theme}`]
-					}`}
-				/>
+				<Background text="marina" style={styles.background} />
 				<Slider
 					slides={[
-						<h1
-							className={`${styles.heading} ${
-								styles[`heading--${theme}`]
-							}`}
-						>
-							Front-end Developer
-						</h1>,
-						<span
-							className={`${styles.heading} ${
-								styles[`heading--${theme}`]
-							}`}
-						>
-							UI/UX Designer
-						</span>,
+						<h1 className={styles.heading}>Front-end Developer</h1>,
+						<span className={styles.heading}>UI/UX Designer</span>,
 					]}
 				/>
-				<Background
-					text="bajic"
-					style={`${styles.background} ${
-						styles[`background--${theme}`]
-					}`}
-				/>
+				<Background text="bajic" style={styles.background} />
 			</div>
 			<div className={styles.btn}>
 				<Button href="#projects" hierarchy="primary" text="My work" />
