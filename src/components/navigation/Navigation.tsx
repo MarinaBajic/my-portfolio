8ceeab888bdac5c7f7818c2151d8219daee2f668
MarinaBajic <marinabajic10@gmail.com>
2024-08-10 22:45:22 +0200

    feat: Active links when section is in view

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import styles from './Navigation.module.scss';
 import navigationData from './navigationData.json';
 
@@ -8,6 +9,32 @@ type TLink = {
 
 const Navigation = () => {
 	const links: Array<TLink> = navigationData;
+
+	useEffect(() => {
+		const sections = document.querySelectorAll('section');
+
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					const id = entry.target.getAttribute('id');
+					const link = document.querySelector(
+						`nav ul li a[href="#${id}"]`,
+					);
+
+					if (entry.isIntersecting) {
+						link?.classList.add(styles.active);
+					} else {
+						link?.classList.remove(styles.active);
+					}
+				});
+			},
+			{ threshold: 0.2 },
+		);
+
+		sections.forEach((section) => observer.observe(section));
+
+		return () => observer.disconnect();
+	}, []);
 
 	return (
 		<nav>
